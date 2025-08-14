f4ed16081ab14c31a4c811bde88cfc516c92ac21
Mansi Katiyar <mansikatiyar01@gmail.com>
2025-08-14 14:38:54 +0530

    Initial commit

---
+++ qr_code.py
@@ -0,0 +1,17 @@
+import qrcode
+
+data = "https://www.example.com"
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=4
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="Pink", back_color="Purple")
+img.save("myqrcode.png")
+print("QR Code saved as myqrcode.png")
+
+
+
